aeb61503e76d9e8203a54673ac94007ce2cf5877
yjq06 <152877519+yjq06@users.noreply.github.com>
2024-09-26 10:59:33 +0800

    Add files via upload

---
+++ 2024.9.26.py
@@ -0,0 +1,13 @@
+# total = float(input("请输入总收入: "))
+# cost_w = float(input("请输入工资: "))
+# cost_e = float(input("请输入餐饮费: "))
+# cost_t = float(input("请输入交通费"))
+# profit = (total-cost_w-cost_e-cost_t)*0.8
+# print("你的总利润为:",str(profit))
+
+# def printlen():
+#     a = input("请输入一个字符串：")
+#     lenghth = len(a)
+#     print("长度为",lenghth)
+
+# printlen()
