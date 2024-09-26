d84d1a59ae04820a4df5501a55ff40c3b62035b8
yjq06 <152877519+yjq06@users.noreply.github.com>
2024-09-26 09:01:12 +0800

    Add files via upload

---
+++ 2024.9.25.py
@@ -0,0 +1,31 @@
+# def name_age(name,age):
+#     print("他的名字是:",name,":","他的年龄是：",age)
+
+# name_age("王",18)
+# 自己的↑
+
+# # 有两个参数
+# def getNameAge(str1, str2):
+#     # 用切片获取第1个参数的人名
+#     name = str1[-2:]
+
+#     # 用切片获取第2个参数的 年龄
+#     age = str2[-2:]
+
+#     # 将人名和 年龄连起来，中间是冒号
+
+#     ret = name + ':' + age
+
+#     # 别忘了， 最后一定要使用return 返回结果
+#     return ret
+
+# # 不指定参数名的调用
+# name_age1 = getNameAge('他的名字是：关羽','他的年龄是：28')
+# # 打印出返回结果
+# print(name_age1)
+
+
+# # 指定参数名的调用
+# name_age2 = getNameAge(str1='他的名字是：赵云',str2='他的年龄是：24')
+# # 打印出返回结果
+# print(name_age2)
