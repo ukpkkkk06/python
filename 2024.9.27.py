f663d1f57889b11cdb30c4d7d5c5f0f15b215751
ukpkkkk06 <152877519+ukpkkkk06@users.noreply.github.com>
2024-09-27 09:49:26 +0800

    Add files via upload

---
+++ 2024.9.27.py
@@ -0,0 +1,14 @@
+# var1 = [33,['ukpkkkk','test'],'hello world!']
+# print(var1[2])
+
+# var1 = [33,['ukpkkkk','test'],'hello world!']
+# print(var1[1][1])
+
+# var1 = [33,['ukpkkkk','test'],'hello world!']
+# var1[2] = "Oh my god"
+# var1[1][1] = "哈吉米"
+# print(var1)
+
+# var1 = [ 33, ('我的名字', '哈吉米'), 'hello world!']
+# var1[1] = ('我的名字', '哇奥')
+# print(var1)
